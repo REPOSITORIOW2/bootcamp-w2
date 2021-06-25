5604f1adfb150c7dc388dabbeffed8013ec9443e
Giovannapls <giovanna.polissici@mercadolivre.com>
2021-06-25 12:58:04 -0300

    ex  aula 4 2.4

---
+++ Giovanna/Aula4Ex2/src/com/melibootcamp/Fracao.java
@@ -0,0 +1,28 @@
+package com.melibootcamp;
+
+public class Fracao {
+    public double adicionar(double valor1, double valor2){
+        return valor1+ valor2;
+    }
+    public double adicionar(double valor1, int valor2){
+        return valor1+ valor2;
+    }
+    public double subtrair(double valor1, double valor2){
+        return valor1 - valor2;
+    }
+    public double subtrair(double valor1, int valor2){
+        return valor1 - valor2;
+    }
+    public double multiplicar(double valor1, double valor2){
+     return valor1 * valor2;
+    }
+    public double multiplicar(double valor1, int valor2){
+        return valor1 * valor2;
+    }
+public double dividir(double valor1, double valor2){
+        return  valor1/valor2;
+}
+    public double dividir(double valor1, int valor2){
+        return  valor1/valor2;
+    }
+}
