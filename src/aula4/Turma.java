acb68e5fe900319301641ce4900598665eabd7a9
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-25 11:53:16 -0300

    Criação da classe Turma

---
+++ src/aula4/Turma.java
@@ -0,0 +1,47 @@
+package aula4;
+
+public class Turma {
+    private String serie;
+    private Character codigoID;
+    private Estudante[] estudantes;
+    private Disciplina[] disciplinas;
+
+    public Turma(String serie, Character codigoID, Estudante[] estudantes, Disciplina[] disciplinas) {
+        this.serie = serie;
+        this.codigoID = codigoID;
+        this.estudantes = estudantes;
+        this.disciplinas = disciplinas;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public Character getCodigoID() {
+        return codigoID;
+    }
+
+    public void setCodigoID(Character codigoID) {
+        this.codigoID = codigoID;
+    }
+
+    public Estudante[] getEstudantes() {
+        return estudantes;
+    }
+
+    public void setEstudantes(Estudante[] estudantes) {
+        this.estudantes = estudantes;
+    }
+
+    public Disciplina[] getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(Disciplina[] disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+}
