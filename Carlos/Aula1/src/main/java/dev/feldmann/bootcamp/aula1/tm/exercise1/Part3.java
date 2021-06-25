6b5a8f91f8e7e1937fa6287ef96711300d1181db
Carlos André Feldmann Júnior <carlos.feldmann@mercadolivre.com>
2021-06-25 11:35:39 -0300

    feat: Aula1

---
+++ Carlos/Aula1/src/main/java/dev/feldmann/bootcamp/aula1/tm/exercise1/Part3.java
@@ -0,0 +1,34 @@
+package dev.feldmann.bootcamp.aula1.tm.exercise1;
+
+import java.util.Scanner;
+
+public class Part3 {
+
+
+    public static void main(String[] args) {
+
+        var scanner = new Scanner(System.in);
+        System.out.println("Informe o número que deseja verificar:");
+        int n = scanner.nextInt();
+
+        if (isPrime(n)) {
+            System.out.println("O número é primo!");
+        } else {
+            System.out.println("Não é primo");
+        }
+
+    }
+
+    private static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = n - 1; i > 1; i--) {
+            if (n % i == 0) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
