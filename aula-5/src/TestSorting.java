007c10e3d1ea464aceb3b257bae89367fd660484
dsnunes07 <daniel.nunes@mercadolivre.com>
2021-06-28 12:51:26 -0300

    test class

--- aula-5/src/TestSorting.java
+++ aula-5/src/TestSorting.java
@@ -3,16 +3,40 @@ package src;
 import java.util.Comparator;
 
 public class TestSorting {
+
+    public static Integer[] generateArray(int n) {
+        Integer[] arr = new Integer[n];
+        int i = 0;
+        while (i < n) {
+            arr[i] = (int) (Math.random() * n);
+            i++;
+        }
+        return arr;
+    }
+
+    public static void elapsedTimeSeconds(long start, long end) {
+        System.out.println((end - start)/100F + " segundos");
+    }
+
     public static void main(String[] args) {
-        Integer[] numbers = {2, 4, 1, 23, 0, 5, 9};
-        QuickSortSorterImple qs = new QuickSortSorterImple();
-        qs.sort(numbers, new Comparator() {
+        int n = 10000;
+        long start;
+        long end;
+        Integer[] arr = generateArray(n);
+        Comparator c = new Comparator() {
             @Override
             public int compare(Object o1, Object o2) {
                 return ((Integer) o1).compareTo((Integer) o2);
             }
-        });
-        for (Integer i : numbers)
-            System.out.println(i);
+        };
+
+        // performance tests
+        System.out.println("Quicksort com " + n + " elementos");
+        QuickSortSorterImple qs = new QuickSortSorterImple();
+        start = System.currentTimeMillis();
+        qs.sort(arr, c);
+        end = System.currentTimeMillis();
+        elapsedTimeSeconds(start, end);
+
     }
 }
