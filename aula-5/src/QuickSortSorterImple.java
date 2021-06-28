e64ae94d8cb1f2affcf9a5d2e8ea688be0f7c24d
CSiqueiraMeli <cleberson.siqueira@mercadolivre.com>
2021-06-28 11:52:53 -0300

    Implementacao dos metodos quickSort e serapar

--- aula-5/src/QuickSortSorterImple.java
+++ aula-5/src/QuickSortSorterImple.java
@@ -1,3 +1,40 @@
+import java.util.Comparator;
+
+import org.graalvm.compiler.asm.sparc.SPARCAssembler.CMOV;
+
 public class QuickSortSorterImple implements Sorter<T> {
-  
+
+    @Override
+    public void sort(T arr[], Comparator<T> c) {
+        quickSort(arr, c);
+    }
+
+    private void quickSort(T arr[], Comparator<T> c, int inicio, int fim) {
+        if (inicio < fim) {
+            int posicaoPivo = separar(arr, c, inicio, fim);
+            quickSort(arr, c, inicio, posicaoPivo - 1);
+            quickSort(arr, c, posicaoPivo + 1, fim);
+        }
+    }
+    private static int separar(T arr[], Comparator<T> c, int inicio, int fim) {
+        T pivo = arr[inicio];
+        int i = inicio + 1, f = fim;
+        while (i <= f) {
+               if (arr[i] <= pivo)
+                      i++;
+               else if (pivo < arr[f])
+                      f--;
+               else {
+                      T troca = arr[i];
+                      arr[i] = arr[f];
+                      arr[f] = troca;
+                      i++;
+                      f--;
+               }
+        }
+        arr[inicio] = arr[f];
+        arr[f] = pivo;
+        return f;
+  }
+
 }
