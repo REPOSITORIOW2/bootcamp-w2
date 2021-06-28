a4f3332b2d8465a964a33838c69685c0b3e1c60b
dsnunes07 <daniel.nunes@mercadolivre.com>
2021-06-28 16:12:08 -0300

    experiments

--- aula-5/project/src/TestSorting.java
+++ aula-5/project/src/TestSorting.java
@@ -8,21 +8,22 @@ public class TestSorting {
         Integer[] arr = new Integer[n];
         int i = 0;
         while (i < n) {
-            arr[i] = (int) (Math.random() * n);
+            arr[i] = n - i;
             i++;
         }
         return arr;
     }
 
-    public static void elapsedTimeSeconds(long start, long end) {
-        System.out.println((end - start)/100F + " segundos");
+    public static void printElapsedTimeSeconds(long elapsedTime) {
+        System.out.println(elapsedTime/100F + " segundos");
     }
 
     public static void main(String[] args) {
-        int n = 10000;
+        int n = 5000;
         long start;
         long end;
         Integer[] arr = generateArray(n);
+        Time time = new Time();
         Comparator c = new Comparator() {
             @Override
             public int compare(Object o1, Object o2) {
@@ -32,20 +33,9 @@ public class TestSorting {
 
         // performance tests
         Sorter sorter = (Sorter) MyFactory.getInstance("");
-        start = System.currentTimeMillis();
+        time.start();
         sorter.sort(arr, c);
-        end = System.currentTimeMillis();
-        elapsedTimeSeconds(start, end);
-
-        arr = generateArray(n);
-
-        Sorter s = new BubbleSortSorterImple();
-
-        s.sort(arr,c);
-
-//        for (Integer i: arr) {
-//            System.out.println(i);
-//        }
-
+        time.stop();
+        printElapsedTimeSeconds(time.elapsedTime());
     }
 }
