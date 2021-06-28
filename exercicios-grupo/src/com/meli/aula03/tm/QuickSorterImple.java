2cc1fec302332a7e0b22559c5ac84d5ee3e73b21
Joao Victor Magalhaes <joao.jmagalhaes@mercadolivre.com>
2021-06-28 17:06:12 -0300

    Changes by mpreccaro

---
+++ exercicios-grupo/src/com/meli/aula03/tm/QuickSorterImple.java
@@ -0,0 +1,55 @@
+package com.meli.aula03.tm;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class QuickSorterImple implements Sorter<Integer> {
+
+
+    //@Override
+    //public void sort(Object[] arr, Comparator c) {
+    //}
+
+    private int[] numbers;
+    private int number;
+
+    @Override
+    public void sort(Integer[] x, Comparator c) {
+
+        int[] values = Arrays.stream(x).mapToInt(Integer::intValue).toArray();
+        if (values ==null || values.length==0){
+            return;
+        }
+        this.numbers = values;
+        number = values.length;
+        quicksort(0, number - 1);
+    }
+
+    private void quicksort(int low, int high) {
+        int i = low, j = high;
+        int pivot = numbers[low + (high-low)/2];
+        while (i <= j) {
+            while (numbers[i] < pivot) {
+                i++;
+            }
+            while (numbers[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                exchange(i, j);
+                i++;
+                j--;
+            }
+        }
+        if (low < j)
+            quicksort(low, j);
+        if (i < high)
+            quicksort(i, high);
+    }
+
+    private void exchange(int i, int j) {
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+    }
+}
