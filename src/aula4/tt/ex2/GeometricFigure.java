226ca4f24e07276cf247454d6123f7e13718d492
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-28 17:00:52 -0300

    Criação da classe abstrata

---
+++ src/aula4/tt/ex2/GeometricFigure.java
@@ -0,0 +1,15 @@
+package aula4.tt.ex2;
+
+public abstract class GeometricFigure {
+
+    public abstract double area();
+
+//    @Override
+//    public String toString() {
+//        return super.toString();
+//    }
+    /*
+    Se tivermos informações suficientes poderíamos ter a sobrescrita do método toString,
+    porém neste cenário nós não vemos tal necessidade de implementação na classe abstrata.
+     */
+}
