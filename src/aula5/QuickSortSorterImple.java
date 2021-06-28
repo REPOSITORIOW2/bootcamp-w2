5a408ddb4d4c9d0ee0b485160f287d04d20e0f79
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-28 10:44:44 -0300

    Criação da classe QuickSortSorterImple

---
+++ src/aula5/QuickSortSorterImple.java
@@ -0,0 +1,11 @@
+package aula5;
+
+import java.util.Comparator;
+
+public class QuickSortSorterImple implements Sorter{
+
+    @Override
+    public void sort(Object[] arr, Comparator c) {
+        
+    }
+}
