cf35c9d91d2fcd9a1524cb9443f5b762f9def03d
“wesleyanazario” <wesleyanazario@gmail.com>
2021-06-28 17:42:27 -0300

    pesonage

---
+++ Aula05/src/br/com/meli/Sword.java
@@ -0,0 +1,21 @@
+package br.com.meli;
+
+public class Sword extends Weapon {
+
+	public Sword() {
+		super("Espada", "Espada", 50, 0, 1);
+	}
+
+	@Override
+	public void action() {
+		System.out.println("Ataque com " + toString());
+
+	}
+
+	@Override
+	public String toString() {
+		return "Sword " + getName() + ", Type :" + getType() + ", Power :" + getPower() + ", Magic :" + getMagic()
+				+ ", getRange :" + getRange();
+	}
+
+}
---
+++ Aula05/src/br/com/meli/Weapon.java
@@ -0,0 +1,64 @@
+package br.com.meli;
+
+public abstract class Weapon {
+	private String name;
+	private String type;
+	private int power;
+	private int magic;
+	private int range;
+
+	public Weapon() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Weapon(String name, String type, int power, int magic, int range) {
+		this.name = name;
+		this.type = type;
+		this.power = power;
+		this.magic = magic;
+		this.range = range;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	public int getMagic() {
+		return magic;
+	}
+
+	public void setMagic(int magic) {
+		this.magic = magic;
+	}
+
+	public int getRange() {
+		return range;
+	}
+
+	public void setRange(int range) {
+		this.range = range;
+	}
+	
+	public abstract void action();
+
+}
