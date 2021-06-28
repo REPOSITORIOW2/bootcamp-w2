f6c7a557a95fdd2462a099659e3b1831e30cec88
Felipe Castro <felipe.fcastro@mercadolivre.com>
2021-06-28 13:57:43 -0300

    add test

--- modulo-5-aula-3-TM/src/Main.java
+++ modulo-5-aula-3-TM/src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.util.Comparator;
 import java.util.Properties;
 
-public class Main {
+public class Main<arrayString> {
     public static void main(String[] args) throws IOException {
 
         Integer[] array = {30, 27, 34, 21, 1, 2, 56, 10};
@@ -18,6 +18,19 @@ public class Main {
 
         for (int i : array) {
             System.out.println(i);
+        }
+
+
+        System.out.println("\n ------------------");
+
+
+        String[] arrayString = {"Felipe", "Castro", "Souza", "Dos", "Santos"};
+
+        Sorter<String> sorterString = (Sorter<String>) MyFactory.getInstance(MyFactory.getSorter());
+        sorterString.sort(arrayString, String::compareTo);
+
+        for(String s : arrayString) {
+            System.out.println(s);
         }
 
     }
