b5dfa2394d9f51ff392ecc4d7d561c5816171a35
dsnunes07 <daniel.nunes@mercadolivre.com>
2021-07-01 11:23:48 -0300

    starting new project for aula8

---
+++ aula8/metrosquadrados/src/test/java/meli/bootcamp/spring/metrosquadrados/MetrosquadradosApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.bootcamp.spring.metrosquadrados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetrosquadradosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
