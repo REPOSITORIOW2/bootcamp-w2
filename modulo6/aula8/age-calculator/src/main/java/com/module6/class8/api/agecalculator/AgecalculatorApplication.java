f3ec64dc78c6ca6060255eff24ac7034f13ef13f
icrxz <igor.cruz@mercadolivre.com>
2021-07-01 15:14:26 -0300

    Class 8 Exercise 2 (Age calculator API)

---
+++ modulo6/aula8/age-calculator/src/main/java/com/module6/class8/api/agecalculator/AgecalculatorApplication.java
@@ -0,0 +1,32 @@
+package com.module6.class8.api.agecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.GregorianCalendar;
+
+@SpringBootApplication
+@RestController
+public class AgecalculatorApplication {
+    private DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgecalculatorApplication.class, args);
+    }
+
+    @GetMapping("/{day}/{month}/{year}/")
+    public String ageCalculator(@PathVariable int day, @PathVariable int month, @PathVariable int year) {
+        LocalDate date = LocalDate.of(year, month, day);
+        Period period = Period.between(date, LocalDate.now());
+
+        return String.valueOf(period.getYears());
+    }
+
+}
---
+++ modulo6/aula8/age-calculator/src/test/java/com/module6/class8/api/agecalculator/AgecalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module6.class8.api.agecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgecalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
