f8aa9654c7db1524199e56eec897f8bbe0f6cd77
dsnunes07 <daniel.nunes@mercadolivre.com>
2021-07-01 14:13:18 -0300

    aula8 idade project

---
+++ aula8/idade/src/test/java/meli/bootcamp/spring/idade/IdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.bootcamp.spring.idade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
