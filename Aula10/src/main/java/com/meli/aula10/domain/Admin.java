58f4177e5251e38aa7cffddf68eb3f49b2fda03d
Augusto Alves Silva <augusto.asilva@mercadolivre.com>
2021-07-05 17:40:53 -0300

    Corrigido erro de polimorfismo do Jackson para List<> no Admin

--- Aula10/src/main/java/com/meli/aula10/domain/Admin.java
+++ Aula10/src/main/java/com/meli/aula10/domain/Admin.java
@@ -1,14 +1,19 @@
 package com.meli.aula10.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.meli.aula10.domain.enums.TipoUsuario;
 
+@JsonTypeName("Admin")
 public class Admin extends Usuario{
+    @JsonProperty("tipoUsuario")
+    private final String tipoUsuario = "Admin";
 
 
     public Admin() {
     }
 
     public Admin(int id, String nome) {
-        super(id, nome, TipoUsuario.ADMIN);
+        super(id, nome);
     }
 }
