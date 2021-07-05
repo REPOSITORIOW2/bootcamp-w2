d79a7879f996ce9e9156c65865c8f3657230b580
icrxz <igor.cruz@mercadolivre.com>
2021-07-05 11:22:02 -0300

    merge branch

---
+++ modulo6/aula10/linkTracker/src/test/java/com/meli/api/link_tracker/LinkTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.api.link_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
